31684ad102b0282234f887e4475ebd45b32b3f7d
Pratibha-98100 <130212205+Pratibha-98100@users.noreply.github.com>
2023-08-09 01:18:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const browserName = navigator.userAgent;
+const version =  navigator.appVersion;
+// console.log(browser);
+// console.log(version);
+
+const box = document.createElement("div");
+box.id = "browser-info";
+box.innerText = "You are using " + browserName + " version " + version;
+document.body.append(box);
